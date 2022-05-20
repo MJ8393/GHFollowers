3cd6dd362cfa30a9fea524896d03fbd18b0ae713
Mekhriddin <jumayevmekhriddin10@gmail.com>
2022-05-20 23:56:52 +0500

    Initial Commit

---
+++ GHFollowers/FavouritesVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavouritesVC.swift
+//  GHFollowers
+//
+//  Created by Mekhriddin on 20/05/22.
+//
+
+import UIKit
+
+class FavouritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Mekhriddin on 20/05/22.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
